ef4e718049981b6c16d0aa5fd7a00a52bc8c937e
JoelV3ig4 <aluno24946@mtorga.edu.pt>
2025-02-11 19:13:20 +0000

    Update code
    Adição do teclado 4x4 e a passe do circuito que vai desligar.
    Alteração no código de acordo com as mudanças.
    Correção de alguns erros no código e no circuito

--- sketch_jan21a.ino
+++ sketch_jan21a.ino
@@ -1,75 +1,123 @@
 /*
-	Programador....:Joel
-    Data...........:21/1/2025
-    Observações....:Alarme da Miguel Torga.
+    Programador....: Joel
+    Data...........: 2/2/2025
+    Observações....: Alarme do Joel para a Miguel Torga
 */
+
+#include <Keypad.h>
+
 const int BAUD_RATE = 9600;
 
-const byte POWER_LED = 2;
-const byte POWER_BUTTON = 3;
+const byte POWER_LED = 12;   
+const byte POWER_BUTTON = 11; 
 const byte PIR_SENSOR = 6;
-const byte BUZZER = 4;
-
+const byte BUZZER = 13;       
 
 const byte STANDBY = 0;
-const byte POWER_ON = 1:
-const byte ALARM_ON = 2:
-const byte PIR_DETECT = 3;
+const byte POWER_ON = 1;
+const byte PIR_DETECT = 2;
 
 const byte DEBUG = true;
 
-byte alarmState
+byte alarmState;
+bool systemActive = false; 
+bool buttonPressed = false; 
 
-  
-void setup(){
-  Serial.begin(BAUD_RATE);
-  
- pinMode(POWER_LED, OUTPUT);
- pinMode(POWER_BUTTON, INPUT);
- pinMode(PIR_SENSOR, INPUT);
- pinMode(BUZZER, OUTPUT);
- 
- alarmState = STANDBY;
+// Configuração do teclado 4x4
+const byte ROWS = 4;
+const byte COLS = 4;
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
 
- digitalWrite(POWER_LED, LOW)
-   
- if(DEBUG) Serial.println("...")
-  	
+byte rowPins[ROWS] = {9, 8, 7, 6};
+byte colPins[COLS] = {5, 4, 3, 2};
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
+const String correctCode = "1234";   // Código correto para desligar o sistema
+String enteredCode = "";             // Armazena o código digitado
+
+void setup(void) {
+    Serial.begin(BAUD_RATE);
+  
+    pinMode(POWER_LED, OUTPUT);
+    pinMode(POWER_BUTTON, INPUT_PULLUP);
+    pinMode(PIR_SENSOR, INPUT);
+    pinMode(BUZZER, OUTPUT);
+
+    alarmState = STANDBY;
+    digitalWrite(POWER_LED, LOW);
+
+    if (DEBUG) Serial.println("Sistema iniciado em modo STANDBY...");
 }
 
-void loop(){
-  switch(alarmState){
-    case STANDBY: {
-      if(DEBUG) Serial.println("Standby...")
-      
-      if(digitalRead(POWER_BUTTON) == false) alarmState = POWER_ON;
-        break;
+void loop(void) {
+    if (digitalRead(POWER_BUTTON) == LOW && !buttonPressed) { // Botão pressionado
+        delay(50); // Debounce
+        if (digitalRead(POWER_BUTTON) == LOW) {
+            toggleSystem();
+            buttonPressed = true;
+        }
+    } else if (digitalRead(POWER_BUTTON) == HIGH) {
+        buttonPressed = false;
     }
-    case POWER_ON: {
-      if(DEBUG) Serial.println("Power ON...")
-        
-      digitalWrite(POWER_lED, LOW);
-      delay(3000);
-      alamState = ALARM_ON 
-      break;
+
+    char key = keypad.getKey();
+    if (key) {
+        enteredCode += key;
+        if (DEBUG) Serial.println(enteredCode);
+
+        if (enteredCode.length() == 4) {
+            if (enteredCode == correctCode) {
+                systemActive = false;
+                alarmState = STANDBY;
+                digitalWrite(POWER_LED, LOW);
+                digitalWrite(BUZZER, LOW);
+                if (DEBUG) Serial.println("Código correto. Sistema DESLIGADO.");
+            } else {
+                if (DEBUG) Serial.println("Código incorreto.");
+            }
+            enteredCode = ""; // Limpa o código após tentativa
+        }
     }
-    case ALARM_ON: {
-      if(DEBUG) Serial.println("Alarme ON...")
-      
-        digitalRead(PIR_SENSOR) == true){
-        alarnState = PIR_DETECT;
-      }
-      break;
-    } 
- 	case PIR_DETECT: {
-      if(DEBUG) Serial.println("PIR DETECT...")
-      for(int i = 700; i <=800; i++){
-        tone(Buzzer);
-        delay(15);
-      }
-        
-        
-       break;
-      }
-  }
+
+    switch (alarmState) {
+        case STANDBY: {
+            digitalWrite(POWER_LED, LOW);
+            break;
+        }
+
+        case POWER_ON: {
+            digitalWrite(POWER_LED, HIGH);
+            if (digitalRead(PIR_SENSOR) == HIGH) {
+                alarmState = PIR_DETECT;
+            }
+            break;
+        }
+
+        case PIR_DETECT: {
+            if (DEBUG) Serial.println("MOVIMENTO DETECTADO! Alarme Ativado...");
+
+            digitalWrite(BUZZER, HIGH);
+
+            break;
+        }
+    }
+}
+
+void toggleSystem() {
+    systemActive = !systemActive;
+    if (systemActive) {
+        alarmState = POWER_ON;
+        if (DEBUG) Serial.println("Sistema LIGADO.");
+    } else {
+        alarmState = STANDBY;
+        digitalWrite(POWER_LED, LOW);
+        digitalWrite(BUZZER, LOW);
+        if (DEBUG) Serial.println("Sistema DESLIGADO.");
+    }
 }
